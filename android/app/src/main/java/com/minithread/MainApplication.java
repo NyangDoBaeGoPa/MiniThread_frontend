add5fd4a7621ae25904925b20e1274ecd7ddcfba
DongHyunKim <akainoo123@gmail.com>
2023-08-25 22:24:30 +0900

    fix : no more react-native-camera-roll

--- android/app/src/main/java/com/minithread/MainApplication.java
+++ android/app/src/main/java/com/minithread/MainApplication.java
@@ -1,5 +1,4 @@
 package com.minithread;
-import com.reactnativecommunity.cameraroll.CameraRollPackage;
 import android.app.Application;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
@@ -25,7 +24,6 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new CameraRollPackage());
           return packages;
         }
 
