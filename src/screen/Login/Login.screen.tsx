a962943c2ed51d915fc08b1826ef46bb793963b6
DongHyunKim <akainoo123@gmail.com>
2023-09-08 16:07:26 +0900

    feat : add login-id-input module

--- src/screen/Login/Login.screen.tsx
+++ src/screen/Login/Login.screen.tsx
@@ -1,12 +1,14 @@
 import {Box, Stack} from '@mobily/stacks';
 import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
+import {FormProvider} from 'react-hook-form';
 import {ImageBackground, Pressable, Text, TextInput, Alert} from 'react-native';
 
 import {RootStackParamList} from '../root.navigator';
 
 import {useLoginForm} from './hooks';
 import {useLoginScreenData} from './login.hook';
+import {LoginIdInputModule} from './modules';
 
 type LoginScreenProps = {};
 
@@ -24,7 +26,7 @@ export const LoginScreen = ({}: LoginScreenProps) => {
   const navigation = useNavigation<LoginScreenNavigationProps>();
   const route = useRoute<LoginScreenNavigationRouteProps>();
 
-  const {} = useLoginForm();
+  const methods = useLoginForm();
 
   const {
     id,
@@ -44,79 +46,64 @@ export const LoginScreen = ({}: LoginScreenProps) => {
   };
 
   return (
-    <Box>
-      <ImageBackground
-        source={{
-          uri: 'https://res.cloudinary.com/dahw1d9li/image/upload/v1692434627/backgroundscreen_dv5tcz.png',
-        }}
-        style={{width: '100%', height: '100%'}}>
-        <Stack
-          paddingX={55}
-          space={20}
-          style={{height: '100%', justifyContent: 'center'}}>
-          <TextInput
-            placeholder="아이디를 입력하세요."
-            value={id}
-            onFocus={() => handleFocusedInput({id: true, password: false})}
-            onBlur={() => handleFocusedInput(null)}
-            onChangeText={handleIdInput}
-            placeholderTextColor="#AAAAAA"
-            style={{
-              width: '100%',
-              textAlign: 'center',
-              backgroundColor: 'white',
-              borderRadius: 20,
-              paddingHorizontal: 40,
-              paddingVertical: 16,
-              fontSize: 12,
-              borderColor: focusedInput?.id ? '#E58634' : 'white',
-              borderWidth: 1,
-            }}
-          />
-          <TextInput
-            placeholder="비밀번호를 입력하세요."
-            value={password}
-            secureTextEntry
-            onFocus={() => handleFocusedInput({id: false, password: true})}
-            onBlur={() => handleFocusedInput(null)}
-            onChangeText={handlePasswordInput}
-            placeholderTextColor="#AAAAAA"
-            style={{
-              width: '100%',
-              textAlign: 'center',
-              backgroundColor: 'white',
-              borderRadius: 20,
-              paddingHorizontal: 40,
-              paddingVertical: 16,
-              fontSize: 12,
-              borderColor: focusedInput?.password ? '#E58634' : 'white',
-              borderWidth: 1,
-            }}
-          />
-          <Box alignX="center" direction="row">
-            <Pressable
-              onPress={handleLogin}
+    <FormProvider {...methods}>
+      <Box>
+        <ImageBackground
+          source={{
+            uri: 'https://res.cloudinary.com/dahw1d9li/image/upload/v1692434627/backgroundscreen_dv5tcz.png',
+          }}
+          style={{width: '100%', height: '100%'}}>
+          <Stack
+            paddingX={55}
+            space={20}
+            style={{height: '100%', justifyContent: 'center'}}>
+            <LoginIdInputModule />
+            <TextInput
+              placeholder="비밀번호를 입력하세요."
+              value={password}
+              secureTextEntry
+              onFocus={() => handleFocusedInput({id: false, password: true})}
+              onBlur={() => handleFocusedInput(null)}
+              onChangeText={handlePasswordInput}
+              placeholderTextColor="#AAAAAA"
               style={{
-                backgroundColor: canLogin ? '#E58634' : '#C4C4C4',
-                paddingHorizontal: 36,
-                paddingTop: 8,
-                paddingBottom: 8,
-                alignItems: 'center',
+                width: '100%',
+                textAlign: 'center',
+                backgroundColor: 'white',
                 borderRadius: 20,
-                justifyContent: 'center',
-                opacity: canLogin ? 1 : 0.5,
+                paddingHorizontal: 40,
+                paddingVertical: 16,
+                fontSize: 12,
+                borderColor: focusedInput?.password ? '#E58634' : 'white',
+                borderWidth: 1,
               }}
-              disabled={!canLogin}>
-              <Text style={{color: 'white', textAlign: 'center', fontSize: 15}}>
-                로그인
-              </Text>
-            </Pressable>
-          </Box>
-          {errorMessage ? (
-            <Text style={{color: 'red'}}>{errorMessage}</Text>
-          ) : null}
-        </Stack>
-      </ImageBackground>
-    </Box>
+            />
+            <Box alignX="center" direction="row">
+              <Pressable
+                onPress={handleLogin}
+                style={{
+                  backgroundColor: canLogin ? '#E58634' : '#C4C4C4',
+                  paddingHorizontal: 36,
+                  paddingTop: 8,
+                  paddingBottom: 8,
+                  alignItems: 'center',
+                  borderRadius: 20,
+                  justifyContent: 'center',
+                  opacity: canLogin ? 1 : 0.5,
+                }}
+                disabled={!canLogin}>
+                <Text
+                  style={{color: 'white', textAlign: 'center', fontSize: 15}}>
+                  로그인
+                </Text>
+              </Pressable>
+            </Box>
+            {errorMessage ? (
+              <Text style={{color: 'red'}}>{errorMessage}</Text>
+            ) : null}
+          </Stack>
+        </ImageBackground>
+      </Box>
+    </FormProvider>
   );
 };
---
+++ src/screen/Login/modules/login-id-input/login-id-input.module.tsx
@@ -0,0 +1,45 @@
+import {Stack} from '@mobily/stacks';
+import {memo, useState} from 'react';
+import {useController, useFormContext} from 'react-hook-form';
+import {TextInput} from 'react-native';
+
+import {LoginForm} from '../../hooks';
+
+import {Text} from '@/atoms';
+
+type LoginIdInputModuleProps = {};
+
+export const LoginIdInputModule = memo<LoginIdInputModuleProps>(() => {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+
+  const {control} = useFormContext<LoginForm>();
+  const {
+    field: {onChange, value},
+    fieldState,
+  } = useController({control, name: 'ID'});
+
+  return (
+    <Stack space={4}>
+      <TextInput
+        placeholder="아이디를 입력하세요."
+        value={value}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        onChangeText={onChange}
+        placeholderTextColor="#AAAAAA"
+        style={{
+          width: '100%',
+          textAlign: 'center',
+          backgroundColor: 'white',
+          borderRadius: 20,
+          paddingHorizontal: 40,
+          paddingVertical: 16,
+          fontSize: 12,
+          borderColor: isFocused ? '#E58634' : 'white',
+          borderWidth: 1,
+        }}
+      />
+      <Text>{fieldState.error?.message}</Text>
+    </Stack>
+  );
+});
